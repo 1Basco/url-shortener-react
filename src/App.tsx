ab7fd9f1b0aba513488c5523d532a716b33d88d8
1Basco <lsos13@gmail.com>
2023-02-16 00:39:04 -0300

    [Fix](Fix code to work with the shenanigans of pages)

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ function App() {
   useEffect(() => {
     // Check if the current path is a key in the map of shortened URLs
     const path = window.location.pathname.replace("/", "");
+
+    const url = window.location.href;
+    const lastSegment = url.substring(url.lastIndexOf("/") + 1);
     // get shortened url from localStorage
     const shortenedUrls = JSON.parse(
       localStorage.getItem("shortenedUrls") || "[]"
     );
 
     // if the current path is a key in the map of shortened URLs
-    if (shortenedUrls[path]) {
-      // redirect to the URL value
-      window.location.href = shortenedUrls[path];
-    }
-    if (shortenedUrls.hasOwnProperty(path)) {
+    // if (shortenedUrls[path]) {
+    //   // redirect to the URL value
+    //   window.location.href = shortenedUrls[path];
+    // }
+    if (shortenedUrls.hasOwnProperty(lastSegment)) {
       // Redirect to the corresponding URL
-      window.location.href = shortenedUrls[path];
+      window.location.href = shortenedUrls[lastSegment];
     }
   }, []);
 
